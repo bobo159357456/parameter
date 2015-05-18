7038c32f5845c10a7ac93378d825eb8289b51c06
liuyunbo <liuyunbo@dhcc.com.cn>
2015-05-18 18:07:26 +0800

    ADD test

---
+++ src/Sqrt.java
@@ -0,0 +1,11 @@
+/**
+ * Created by bobo on 2015/5/18.
+ */
+public class Sqrt {
+    public static void main(String[] args){
+        for (int i = 1; i < 20; i++) {
+            double root = Math.sqrt(i);
+            System.out.println("sqrt("+i+")="+root);
+        }
+    }
+}
